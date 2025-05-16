28d998f265f06e93ec70c7231ced86e6e2f535a9
kev <fbian.kevin@gmail.com>
2025-05-16 17:29:28 +0800

    Add tests for PlanetService and update Cucumber integration configuration

--- springboot-testcontainer-cucumber/app/src/test/java/com/fabiankevin/app/AppApplicationTests.java
+++ springboot-testcontainer-cucumber/app/src/test/java/com/fabiankevin/app/AppApplicationTests.java
@@ -1,13 +1,30 @@
 package com.fabiankevin.app;
 
+import com.fabiankevin.app.service.PlanetService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class AppApplicationTests {
+    @Autowired
+    private PlanetService planetService;
 
     @Test
-    void contextLoads() {
+    void contextLoads1() {
+        assertDoesNotThrow(() -> {
+            planetService.processPlanet(null);
+        });
     }
+
+    @Test
+    void contextLoads2() {
+        assertDoesNotThrow(() -> {
+            planetService.processPlanet(null);
+        });
+    }
+
 
 }
